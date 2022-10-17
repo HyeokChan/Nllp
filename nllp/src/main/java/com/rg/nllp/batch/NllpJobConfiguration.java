6199c890184d964a05248ff0577b219906b5ffd2
kwonhyeokchan <qpjiq123@gmail.com>
2022-10-18 01:49:31 +0900

    batch parameter increment

--- nllp/src/main/java/com/rg/nllp/batch/NllpJobConfiguration.java
+++ nllp/src/main/java/com/rg/nllp/batch/NllpJobConfiguration.java
@@ -6,12 +6,19 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Configuration
@@ -20,20 +27,24 @@ public class NllpJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+
     @Bean
     public Job helloJob(){
+        String uuid = UUID.randomUUID().toString();
         return jobBuilderFactory.get("helloJob")
-                .start(helloStepStart())
+                .start(helloStepStart(null))
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
-    public Step helloStepStart(){
+    @JobScope
+    public Step helloStepStart(@Value("#{jobParameters[date]}") String date){
         return stepBuilderFactory.get("helloStepStart")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        log.info("Hello Spring Batch !!");
+                        log.info(">>>>> Hello Spring Batch");
                         return RepeatStatus.FINISHED;
                     }
                 })
