8d70a95f53140d4dd3ab4b5d352fdb5ef91ef374
kwonhyeokchan <qpjiq123@gmail.com>
2022-10-17 23:54:30 +0900

    batch init

---
+++ nllp/src/main/java/com/rg/nllp/batch/NllpJobConfiguration.java
@@ -0,0 +1,42 @@
+package com.rg.nllp.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+@Slf4j
+public class NllpJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob(){
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStepStart())
+                .build();
+    }
+
+    @Bean
+    public Step helloStepStart(){
+        return stepBuilderFactory.get("helloStepStart")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        log.info("Hello Spring Batch !!");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
